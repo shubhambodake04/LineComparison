a1450f58a4ac027c86eb665e9b6d05d79cd65897
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-23 14:31:34 +0530

    Added in master

--- LineComparison/Distance.cs
+++ LineComparison/Distance.cs
@@ -6,6 +6,20 @@ namespace LineComparison
 {
     public class Distance
     {
-        
+        int x1, x2, y1, y2;
+        public Distance(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+
+        }
+        public double Compair()
+        {
+            double d = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return d;
+        }
+
     }
 }
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,7 +6,9 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-           
+            Distance obj = new Distance(1, 2, 1, 2);
+            double LineOne = obj.Compair();
+            Console.WriteLine(LineOne);
         }
     }
 }
